572e0e8b8897cd1e3f616b60a1cec06177149de9
yeon <mail@yeons.io>
2024-05-05 06:57:13 -0400

    build(deps): add commander for cli

--- cli/cli.ts
+++ cli/cli.ts
@@ -1,1 +1,12 @@
-console.log('hello world from cli')
+import { Command } from 'commander'
+
+const program = new Command()
+
+program
+	.name('spark')
+	.description('CLI tool for managing Spark libraries')
+	.version('0.0.1')
+
+program.parse()
+
+console.log('parsed commands', program.args)
