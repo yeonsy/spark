43f9a9565e9977ae1da1c2ea1462638353de20fe
yeon <mail@yeons.io>
2024-05-20 16:56:43 -0400

    feat: load index from XDG_CACHE_HOME

--- core/index.ts
+++ core/index.ts
@@ -1,4 +1,4 @@
-import { readFile } from '@core/io/file.js'
+import { readIndexFile } from '@core/io/file.js'
 import { parse } from 'yaml'
 
 export class Index {
@@ -6,12 +6,16 @@ export class Index {
 
 	// todo: typing
 	constructor(yaml: any) {
-		this.#index = yaml.libraries
-		console.log(`loaded index: ${JSON.stringify(this.#index)}`)
+		if (yaml && yaml.libraries) {
+			this.#index = yaml.libraries
+			console.log(`loaded index: ${JSON.stringify(this.#index)}`)
+		} else {
+			console.log('no libraries found in index')
+		}
 	}
 
-	static async fromFile(path: string) {
-		const file = await readFile(path)
+	static async fromFile(path?: string) {
+		const file = await readIndexFile(path)
 		const yaml = parse(file)
 
 		return new Index(yaml)
--- core/io/file.ts
+++ core/io/file.ts
@@ -2,10 +2,21 @@ import * as fs from 'node:fs/promises'
 
 const indexFileName = 'index.yaml'
 
+const getCacheDir = () => {
+	const dir = process.env.XDG_CACHE_HOME || `${process.env.HOME}/.cache`
+	return `${dir}/spark`
+}
+
 export const readFile = (path: string) => {
 	return fs.readFile(path, 'utf8')
 		.catch(err => {
 			console.error(err)
 			throw err
 		})
+}
+
+export const readIndexFile = (path?: string) => {
+	const resolvedPath = path || `${getCacheDir()}/${indexFileName}`
+	console.log(`reading index file from ${resolvedPath}`)
+	return readFile(resolvedPath)
 }
