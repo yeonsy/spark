faced9db8bf942954ae109c3581934abd5e80c1a
yeon <mail@yeons.io>
2024-05-26 18:44:33 -0400

    feat: library tracks items

--- core/library.ts
+++ core/library.ts
@@ -1,34 +1,34 @@
-import { mkdir } from 'node:fs/promises'
-
-// todo: maybe make this configurable?
-const LIBRARY_PATH = '.spark/libaries'
-
-export const getLibraryPath = (root: string, name: string) => {
-	return `${root}/${LIBRARY_PATH}/${name}`
-}
-
-export const createLibrary = async (root: string, name: string) => {
-	// recursive mkdir does not throw if the directory already exists
-	// maybe worth checking if the directory exists ourselves?
-	const path = getLibraryPath(root, name)
-	await mkdir(path, { recursive: true })
-	console.log(`Created new libary at ${path}`)
-}
-
 export class Library {
-	root: string
 	name: string
+	#items: { [key: string]: any }
 
-	constructor(root: string, name: string) {
-		this.root = root
+	// todo: types
+	constructor(name: string, items?: any) {
 		this.name = name
+		this.#items = items || {}
 	}
 
-	get path() {
-		return getLibraryPath(this.root, this.name)
+	getName() {
+		return this.name
 	}
 
-	async init() {
-		await createLibrary(this.root, this.name)
+	getItem(name: string) {
+		return this.#items[name]
+	}
+
+	hasItem(name: string) {
+		return !!this.#items[name]
+	}
+
+	addItem(name: string, item: any) {
+		this.#items[name] = item
+	}
+
+	removeItem(name: string) {
+		delete this.#items[name]
+	}
+
+	getItems() {
+		return this.#items
 	}
 }
