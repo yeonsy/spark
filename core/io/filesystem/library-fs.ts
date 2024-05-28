1d2dec6918cf8cade3d292026565a55ed5ce90d5
yeon <mail@yeons.io>
2024-05-28 00:49:05 -0400

    feat: read library from filesystem

--- core/io/filesystem/library-fs.ts
+++ core/io/filesystem/library-fs.ts
@@ -1,4 +1,6 @@
-import { mkdir } from 'node:fs/promises'
+import { mkdir, readdir, readFile } from 'node:fs/promises'
+import { readFileYaml } from '@io/filesystem/file.js'
+import { Library } from '@core/library.js'
 
 // todo: maybe make this configurable?
 const LIBRARY_PATH = '.spark/libaries'
@@ -13,4 +15,20 @@ export const createLibrary = async (root: string, name: string) => {
 	const path = getLibraryPath(root, name)
 	await mkdir(path, { recursive: true })
 	console.log(`Created new libary at ${path}`)
+}
+
+export const readLibrary = async (root: string, name: string) => {
+	const path = getLibraryPath(root, name)
+	const entries	= await readdir(path, { withFileTypes: true })
+	const files = entries.filter(entry => entry.isFile())
+
+	const items: Record<string, any> = {}
+
+	for (const file of files) {
+		const filepath = `${path}/${file.name}`
+		const content = await readFileYaml(filepath)
+		items[file.name] = content
+	}
+
+	return new Library(name, items)
 }
